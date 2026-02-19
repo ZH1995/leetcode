f2e4382fe3598e99e7f13328f7424e7a9686d227
zh1995 <rwzsjfm@qq.com>
2026-02-19 16:48:00 +0800

    update 238

--- algorithms/238.除自身以外数组的乘积.cpp
+++ algorithms/238.除自身以外数组的乘积.cpp
@@ -1,40 +1,36 @@
-#include <iostream>
+#include<iostream>
 #include <vector>
 using namespace std;
 
-/*
-
-功能：数组中除当前元素外，其余元素的乘积。
-
-难点：不允许用除法，时间复杂度O(n)。
-
-思路：先从左往右遍历，计算每个元素左侧所有元素的成绩；
-     再从右往左遍历，计算每个元素右侧所有元素的乘积；
-     最后遍历数组，将每个元素左右侧乘积相乘即可。
-*/
 class Solution {
 public:
+    // 时间复杂度O(n)，空间复杂度O(n)
     vector<int> productExceptSelf(vector<int>& nums) {
-        int len = nums.size(), left = 1, right = 1;
-        vector<int> ans(len, 1);
-        for (int i = 1; i < len; i ++) {
-            left *= nums[i-1];
-            ans[i] = left;
+        int size = nums.size();
+        if (size <= 0) return vector<int>{};
+        vector<int> prefix(size + 1, 1), suffix(size + 1, 1), ans;
+        for (int i = 0; i < size; i ++) {
+            prefix[i+1] = prefix[i] * nums[i]; 
         }
-        for (int i = len - 2; i >= 0; i --) {
-            right *= nums[i+1];
-            ans[i] *= right;
+        for (int i = size - 1; i >= 0; i --) {
+            suffix[i] = suffix[i+1] * nums[i];
+        }
+        for (int i = 0; i < size; i ++) {
+            ans.push_back(prefix[i] * suffix[i+1]);
         }
         return ans;
     }
 };
 
-int main() {
-    vector<int> nums = {1,2,3,4};
-    Solution sol;
-    vector<int> ans = sol.productExceptSelf(nums);
-    for (auto item: ans)
-        cout << item << " ";
+void showVector(vector<int> nums) {
+    for (auto x : nums)
+        cout << x << " ";
     cout << endl;
-    return 0; 
+}
+
+int main() {
+    Solution s;
+    vector<int> nums = {1,2,3,4};
+    vector<int> output = s.productExceptSelf(nums);
+    showVector(output);
 }
