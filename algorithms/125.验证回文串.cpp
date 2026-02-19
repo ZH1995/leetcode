da66cf13d12a9eed2c0a20806ad8b101e6d366f7
zh1995 <rwzsjfm@qq.com>
2026-02-19 18:03:14 +0800

    update 125

--- algorithms/125.验证回文串.cpp
+++ algorithms/125.验证回文串.cpp
@@ -1,53 +1,44 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-/*
-
-思路：处理掉非数字字母字符，并将大写字母都转为小写。
-     最后双指针遍历，检查是否满足回文串的条件。
-
-推荐：使用解法二，更清晰优雅。
-*/
 class Solution {
 public:
-    /*
-    // 解法一：
+    /* 解法一：时间复杂度O(n),空间复杂度O(n)
     bool isPalindrome(string s) {
-        int n = s.size(), i, j, newLen = 0;
-        for (int i = 0; i < n; i ++) {
-            if (isalpha(s[i])) {
-                if (s[i] >= 'A' && s[i] <= 'Z') s[i] += 32;
-                s[newLen++] = s[i];
-            } else if (isdigit(s[i])) {
-                s[newLen++] = s[i];
+        string ret = "";
+        for (char c : s) {
+            if (isalpha(c) || isdigit(c)) {
+                if (isupper(c)) c = tolower(c);
+                ret += c;
             }
         }
-        for (i = 0, j = newLen - 1; i < j; i ++, j --) {
-            if (s[i] != s[j]) return false;
-        }
+        int size = ret.size();
+        for (int i = 0, j = size - 1; i < j ; i ++, j --)
+            if (ret[i] != ret[j])
+                return false;
         return true;
     }
     */
-    // 解法二：
+    // 解法二：时间复杂度O(n),空间复杂度O(1)
     bool isPalindrome(string s) {
-        int i = 0, j = s.size() - 1;
-        while (i < j) {
-            while (i < j && !isalnum(s[i])) i ++;
-            while (i < j && !isalnum(s[j])) j --;
-            if (tolower(s[i]) != tolower(s[j])) return false;
-            i ++;
-            j --;
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) left ++;
+            while (left < right && !isalnum(s[right])) right --;
+            if (left < right) {
+                if (tolower(s[left]) != tolower(s[right]))
+                    return false;
+            }
+            left ++;
+            right --;
         }
         return true;
     }
 };
 
 int main() {
-    string s = "A man, a plan, a canal: Panama";
-    //string s = "race a car";
-    //string s = "0P";
     Solution sol;
-    bool ans = sol.isPalindrome(s);
-    cout << ans << endl;
-    return 0; 
+    string s = "A man, a plan, a canal: Panama";
+    cout << sol.isPalindrome(s) << endl;
 }
